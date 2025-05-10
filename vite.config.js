23be5fd0ef36ac9805198dcb99de3e19aafcf2b0
JACK LIN <cvse00566@gmail.com>
2025-05-10 17:01:46 +0800

    調整環境配置

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: 'learnandgrow_admin', // Apache VirtualHost 名稱
+        port: 5173,                 // 預設即可
+        strictPort: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
