609eb7652ab2c88cfa85a7257bbeb89ae52ee14a
JACK LIN <cvse00566@gmail.com>
2025-05-12 21:36:39 +0800

    忘記密碼/建立帳號頁面

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -4,7 +4,7 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
+import { Link, Head, useForm } from '@inertiajs/vue3';
 
 defineProps({
     status: {
@@ -22,40 +22,54 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Forgot Password" />
+    <Head title="忘記密碼" />
 
-        <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-            link that will allow you to choose a new one.
-        </div>
+    <div class="min-h-screen bg-blue-600 flex items-center justify-center">
+        <div class="w-full max-w-md">
+            <div class="bg-white shadow-lg rounded-lg mt-10">
+                <div class="border-b px-4 py-2">
+                    <h3 class="text-center text-2xl font-semibold text-gray-700">密碼恢復</h3>
+                </div>
+                <div v-if="status" class="text-center mb-4 font-medium text-sm text-green-600">
+                    {{ status }}
+                </div>
+                <div class="px-4 py-2">
+                    <p class="text-sm text-gray-500 mb-4">
+                        輸入您的電子郵件地址，我們將向您發送重設密碼的連結。
+                    </p>
+                    <form @submit.prevent="submit">
+                        <div class="mb-4">
+                            <InputLabel for="email" value="Email 信箱" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="mt-1 block w-full"
+                                v-model="form.email"
+                                placeholder="name@example.com"
+                                required
+                                autofocus
+                                autocomplete="username"
+                            />
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
+                        <div class="flex items-center justify-between mt-6">
+                            <Link href="/" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <span>回登入</span>
+                            </Link>
+                            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                電子郵件密碼重設鏈接
+                            </PrimaryButton>
+                        </div>
+                    </form>
+                    <div class="border-t text-center mt-4 py-2 text-sm text-gray-600">
+                        <Link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span>註冊帳號</span>
+                        </Link>
+                    </div>
+                </div>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+        </div>
+    </div>
 </template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -21,83 +21,92 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Register" />
+    <Head title="建立帳戶" />
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="name" value="Name" />
+    <div class="min-h-screen bg-blue-600 flex items-center justify-center">
+        <div class="w-full max-w-3xl px-4">
+            <div class="bg-white shadow-lg rounded-lg p-6 mt-10">
+                <h3 class="text-2xl font-semibold text-center mb-6">建立帳戶</h3>
+                <form @submit.prevent="submit">
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                        <div>
+                            <InputLabel for="name" value="Name" />
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                            <TextInput
+                                id="name"
+                                type="text"
+                                class="mt-1 block w-full"
+                                v-model="form.name"
+                                placeholder="name"
+                                required
+                                autofocus
+                                autocomplete="name"
+                            />
 
-                <InputError class="mt-2" :message="form.errors.name" />
+                            <InputError class="mt-2" :message="form.errors.name" />
+                        </div>
+                        <div>
+                            <InputLabel for="email" value="Email 信箱" />
+
+                            <TextInput
+                                id="email"
+                                type="email"
+                                class="mt-1 block w-full"
+                                v-model="form.email"
+                                placeholder="name@example.com"
+                                required
+                                autocomplete="username"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                        <div>
+                            <InputLabel for="password" value="密碼" />
+
+                            <TextInput
+                                id="password"
+                                type="password"
+                                class="mt-1 block w-full"
+                                v-model="form.password"
+                                placeholder="password"
+                                required
+                                autocomplete="new-password"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.password" />
+                        </div>
+                        <div>
+                            <InputLabel for="password_confirmation" value="確認密碼" />
+
+                            <TextInput
+                                id="password_confirmation"
+                                type="password"
+                                class="mt-1 block w-full"
+                                v-model="form.password_confirmation"
+                                placeholder="confirmation password"
+                                required
+                                autocomplete="new-password"
+                            />
+
+                            <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                        </div>
+                    </div>
+                    <div class="flex items-center justify-end mt-4">
+                        <Link
+                            :href="route('login')"
+                            class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            回登入
+                        </Link>
+
+                        <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            建立帳戶
+                        </PrimaryButton>
+                    </div>
+                </form>
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Already registered?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+        </div>
+    </div>
 </template>
